4fd5372a2e8eecb951be96ad4a0b161e5442df43
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-18 22:18:49 -0300

    feat: adds header customization

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,36 +16,48 @@ function Header({ title, notSearchIcon }) {
   );
 
   return (
-    <div>
-      <button
-        type="button"
-        data-testid="profile-btn"
-        onClick={ profileRoute }
-      >
-        <img
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-          alt="Icon Profile"
-        />
-      </button>
-      <h1 data-testid="page-title">{ title }</h1>
-      { !notSearchIcon && (
-        <button
-          type="button"
-          data-testid="search-btn"
-          onClick={ handleVisivelInput }
-        >
-          <img
-            data-testid="search-top-btn"
-            src={ searchIcon }
-            alt="search-btn"
-          />
-        </button>
-      )}
-      {inputVisivel && (
-        <SearchBar title={ title } />
-      )}
-    </div>
+    <section>
+      <div className="flex justify-between p-2 bg-yellow-300 mb-3 items-center">
+        <img src="logo.svg" alt="logo" width="40px" className="ml-4" />
+        <h4 className="mt-2">Tchuplim Receitas</h4>
+        <div>
+          { !notSearchIcon && (
+            <button
+              type="button"
+              data-testid="search-btn"
+              onClick={ handleVisivelInput }
+            >
+              <img
+                data-testid="search-top-btn"
+                src={ searchIcon }
+                alt="search-btn"
+              />
+            </button>
+          ) }
+          <button
+            type="button"
+            data-testid="profile-btn"
+            onClick={ profileRoute }
+            className="ml-8 mr-4"
+          >
+            <img
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              alt="Icon Profile"
+            />
+          </button>
+        </div>
+      </div>
+      <div>
+        { inputVisivel && (
+          <SearchBar title={ title } />
+        ) }
+      </div>
+      <div className="flex items-center justify-center">
+        <img src="meal.svg" alt="meal-icon" width="60px" className="mr-5" />
+        <h1 data-testid="page-title" className="text-center mt-3">{ title }</h1>
+      </div>
+    </section>
   );
 }
 
