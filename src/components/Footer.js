844b2f1e0c9eaf154cbe744025d064b762ff7ae3
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-18 23:12:05 -0300

    feat: adds footer customization

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,26 @@
 import { Link } from 'react-router-dom';
 
-import drinkIcon from '../images/drinkIcon.svg';
-import mealIcon from '../images/mealIcon.svg';
-
 function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer
+      data-testid="footer"
+      className="flex bg-violet-600 min-w-full justify-between p-2"
+    >
       <Link to="/drinks">
         <img
           data-testid="drinks-bottom-btn"
-          src={ drinkIcon }
+          src="cocktail.png"
           alt="Ícone de Bebida"
+          className="ml-3"
         />
       </Link>
 
       <Link to="/meals">
         <img
           data-testid="meals-bottom-btn"
-          src={ mealIcon }
+          src="meal.png"
           alt="Ícone de Comida"
+          className="mr-3"
         />
       </Link>
     </footer>
