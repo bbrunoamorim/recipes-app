ebbb10c8499b5b3cea90d70a75ca7bb0da9b3f15
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-20 16:07:01 -0300

    chore: removes commented unused code

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -40,31 +40,6 @@ function Provider({ children }) {
 
   const firstLoadRecipesDone = useCallback(() => {
     const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
-    // const doneRecipes = [
-    //   {
-    //     id: 52977,
-    //     type: 'meal',
-    //     nationality: 'Turkish',
-    //     category: 'Side',
-    //     alcoholicOrNot: '',
-    //     name: 'Corba',
-    //     image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-    //     doneDate: '28/10/2020',
-    //     tags: ['Soup', 'liquid'],
-    //   },
-    //   {
-    //     id: 15997,
-    //     type: 'drink',
-    //     nationality: '',
-    //     category: 'Ordinary Drink',
-    //     alcoholicOrNot: 'Optional alcohol',
-    //     name: 'GG',
-    //     image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-    //     doneDate: '28/10/2020',
-    //     tags: ['Soup'],
-    //   },
-    // ];
-
     setDonesRecipes(doneRecipes);
     setFilteredDonesRecipes(doneRecipes);
   }, []);
