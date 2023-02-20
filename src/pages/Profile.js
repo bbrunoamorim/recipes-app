60ecbb764bf97e5e468dd2e597d56cc35ad96943
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-20 14:47:20 -0300

    feat: adds profile page customization

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -14,36 +14,35 @@ function Profile() {
   };
 
   return (
-    <div>
+    <div className="bg-zinc-50 min-h-screen">
       <Header title="Profile" notSearchIcon />
-      <span data-testid="profile-email">{ user.email }</span>
-
-      <section>
+      <div className="flex flex-col items-center my-3 divide-y-2 divide-gray-600">
+        <p className="my-5 text-lg font-medium">{ user.email }</p>
         <button
           type="button"
           data-testid="profile-done-btn"
           onClick={ () => history.push('/done-recipes') }
+          className=" mt-5 mb-3 p-2 bg-violet-400 text-gray-200 font-bold rounded-lg w-60"
         >
           Done Recipes
         </button>
-
         <button
           type="button"
           data-testid="profile-favorite-btn"
           onClick={ () => history.push('/favorite-recipes') }
+          className="my-3 p-2 bg-violet-400 text-gray-200 font-bold rounded-lg w-60"
         >
           Favorite Recipes
         </button>
-
         <button
           type="button"
           data-testid="profile-logout-btn"
           onClick={ logoutProfile }
+          className="my-3 p-2 bg-violet-400 text-gray-200 font-bold rounded-lg w-60"
         >
           Logout
         </button>
-      </section>
-
+      </div>
       <Footer />
     </div>
   );
