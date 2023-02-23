0d4f3bd166ee1d49a434cd3811ff5b5f236b8c03
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-23 15:56:02 -0300

    update: updates profile page css

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function Profile() {
+export default function Profile() {
   const history = useHistory();
 
   const user = JSON.parse(localStorage.getItem('user')) || {};
@@ -14,15 +14,16 @@ function Profile() {
   };
 
   return (
-    <div className="bg-zinc-50 min-h-screen">
+    <div className="font-outfit">
       <Header title="Profile" notSearchIcon />
       <div className="flex flex-col items-center my-3 divide-y-2 divide-gray-600">
-        <p className="my-5 text-lg font-medium">{ user.email }</p>
+        <p className="my-5 text-lg font-medium text-orange-500">{ user.email }</p>
         <button
           type="button"
           data-testid="profile-done-btn"
           onClick={ () => history.push('/done-recipes') }
-          className=" mt-5 mb-3 p-2 bg-violet-400 text-gray-200 font-bold rounded-lg w-60"
+          className=" mt-5 mb-3 p-2 bg-emerald-300 rounded-lg w-60 hover:bg-emerald-400
+          hover:font-semibold transition-all duration-150 cursor-pointer"
         >
           Done Recipes
         </button>
@@ -30,7 +31,8 @@ function Profile() {
           type="button"
           data-testid="profile-favorite-btn"
           onClick={ () => history.push('/favorite-recipes') }
-          className="my-3 p-2 bg-violet-400 text-gray-200 font-bold rounded-lg w-60"
+          className="my-3 p-2 bg-emerald-300 rounded-lg w-60 hover:bg-emerald-400
+          hover:font-semibold transition-all duration-150 cursor-pointer"
         >
           Favorite Recipes
         </button>
@@ -38,7 +40,8 @@ function Profile() {
           type="button"
           data-testid="profile-logout-btn"
           onClick={ logoutProfile }
-          className="my-3 p-2 bg-violet-400 text-gray-200 font-bold rounded-lg w-60"
+          className="my-3 p-2 bg-emerald-300 rounded-lg w-60 hover:bg-emerald-400
+          hover:font-semibold transition-all duration-150 cursor-pointer"
         >
           Logout
         </button>
@@ -47,5 +50,3 @@ function Profile() {
     </div>
   );
 }
-
-export default Profile;
